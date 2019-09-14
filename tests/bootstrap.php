a524c6bc80cd54ff2ef1a0e45085f88dc945c4f7
Choraimy Kroonstuiver <choraimy@23g.nl>
2019-09-14 11:07:35 +0200

    🔧 Remove test bootstrapping
    Which is no longer necessary because only PHPUnit 6+ is supported

--- tests/bootstrap.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-require_once __DIR__ . '/../vendor/autoload.php';
-
-if (!class_exists('PHPUnit\Framework\TestCase')) {
-    class_alias('PHPUnit_Framework_TestCase', 'PHPUnit\Framework\TestCase');
-}
